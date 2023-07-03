657e4d254723d7b4f9e0f42fb1443358eea09bd4
Jean Paul Tabone <jean.paul.tabone@outlook.com>
2023-07-03 15:18:18 +0200

    Configured all the basics of webpack

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*****************************!*\
+  !*** ./src/client/index.js ***!
+  \*****************************/
+alert("Hello");
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
