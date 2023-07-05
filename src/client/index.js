a484496c19d6a036bd27a2dc1a7b2a931487328d
Jean Paul Tabone <jean.paul.tabone@outlook.com>
2023-07-05 19:15:03 +0200

    commenting more code

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,10 @@
 // Import the main SCSS stylesheet
 import "./styles/styles.scss";
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
+import "./styles/resets.scss";
 
 // Import the handleSubmit function from the "./js/handleSubmit" module
 import { handleSubmit } from "./js/handleSubmit";
